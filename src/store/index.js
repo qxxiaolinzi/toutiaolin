d80156a9248fbcf7558a73f7f24dad33658a1a98
lin <2284309229@qq.com>
2022-09-07 17:23:04 +0800

    主分支创建asset

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    tokenObj: {}
+  },
+  getters: {},
+  mutations: {
+    SET_TOKEN(state, token) {
+      state.tokenObj = token
+    }
+  }
+})
